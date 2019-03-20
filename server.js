9bd713a7561dcc6400b625877f1bfbe785136bb5
Brian Illes <brianrilles@gmail.com>
2019-03-20 15:11:18 -0700

    adds custom middleware to ensure the user's name (or names) is upper-cased

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const postsRouter = require("./posts/posts-router.js");
 const server = express();
 
 server.use(express.json());
+server.use(makeUppercase);
 
 server.get("/", (req, res) => {
   res.send(`
@@ -14,7 +15,26 @@ server.get("/", (req, res) => {
 });
 
 // routing
-server.use("/api/users", usersRouter);
+server.use("/api/users", makeUppercase, usersRouter);
 server.use("/api/posts", postsRouter);
+
+function makeUppercase(req, res, next) {
+  const str = req.body.name;
+  const strArrOriginal = str
+    .split(" ")
+    .map(name => name.charAt(0))
+    .join();
+  const strArrUpper = str
+    .split(" ")
+    .map(name => name.charAt(0).toUpperCase())
+    .join();
+  console.log(strArrOriginal);
+  console.log(strArrUpper);
+  if (strArrOriginal == strArrUpper) {
+    next();
+  } else {
+    res.status(401).send("name must be capitalized, try again");
+  }
+}
 
 module.exports = server;
